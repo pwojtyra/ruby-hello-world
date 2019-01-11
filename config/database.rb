eb181de61021db745197d7507f6a21c5842ef651
Pawel Wojtyra <14237103+pwojtyra@users.noreply.github.com>
2019-01-11 14:06:25 +0100

    restore original db file

--- config/database.rb
+++ config/database.rb
@@ -4,16 +4,15 @@ def self.connect_to_database_prod
   begin
     config = {
       :adapter  => "mysql2",
-      :host     => "{ENV["DATABASE_SERVICE_HOST"]}",
-      :port     => "{ENV["DATABASE_SERVICE_PORT"]}",
-      :database => "{ENV["MYSQL_DATABASE"]}"
+      :host     => "#{ENV["DATABASE_SERVICE_HOST"]}",
+      :port     => "#{ENV["DATABASE_SERVICE_PORT"]}",
+      :database => "#{ENV["MYSQL_DATABASE"]}"
     }
     if ENV.key?("MYSQL_ROOT_PASSWORD")
-      config[:username] = "{ENV["MYSQL_USER"]}"
-      config[:password] = "{ENV["MYSQL_ROOT_PASSWORD"]}"
+      config[:password] = "#{ENV["MYSQL_ROOT_PASSWORD"]}"
     else
-      config[:username] = "{ENV["MYSQL_USER"]}"
-      config[:password] = "{ENV["MYSQL_PASSWORD"]}"
+      config[:username] = "#{ENV["MYSQL_USER"]}"
+      config[:password] = "#{ENV["MYSQL_PASSWORD"]}"
     end
 
     puts "Connecting to production database (#{config[:username]}@#{config[:host]}:#{config[:port]})..."
@@ -38,7 +37,6 @@ def self.connect_to_database_test
       :database => "#{ENV["MYSQL_DATABASE"]}"
     }
     if ENV.key?("MYSQL_ROOT_PASSWORD")
-      config[:username] = ENV["MYSQL_USER"]
       config[:password] = ENV["MYSQL_ROOT_PASSWORD"]
     else
       config[:username] = ENV["MYSQL_USER"]
